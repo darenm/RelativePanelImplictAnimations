c69a57cd89c25473939642423b40403741a7456a
Daren May <daren.may@gmail.com>
2016-12-02 14:17:09 -0700

    Add project files.

---
+++ RelativePanelImplicitAnimations/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Numerics;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Media.Animation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace RelativePanelImplicitAnimations
+{
+    /// <summary>
+    ///     An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ImplicitAnimationCollection _implicitAnimations;
+
+        public MainPage()
+        {
+            Timeline.AllowDependentAnimations = true;
+            InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            EnsureImplicitAnimations();
+            var elementVisual = ElementCompositionPreview.GetElementVisual(CommentsScrollviewer);
+            elementVisual.ImplicitAnimations = _implicitAnimations;
+            Poster.Visual.ImplicitAnimations = _implicitAnimations;
+        }
+
+        private void EnsureImplicitAnimations()
+        {
+            if (_implicitAnimations == null)
+            {
+                var compositor =
+                    ElementCompositionPreview.GetElementVisual(this).Compositor;
+
+                var offsetAnimation = compositor.CreateVector3KeyFrameAnimation();
+                offsetAnimation.Target = nameof(Visual.Offset);
+                offsetAnimation.InsertExpressionKeyFrame(1.0f, "this.FinalValue");
+                offsetAnimation.Duration = TimeSpan.FromMilliseconds(200);
+
+                var sizeAnimation = compositor.CreateVector2KeyFrameAnimation();
+                sizeAnimation.Target = nameof(Visual.Size);
+                sizeAnimation.InsertExpressionKeyFrame(1.0f, "this.FinalValue");
+                sizeAnimation.Duration = TimeSpan.FromMilliseconds(200);
+
+                _implicitAnimations = compositor.CreateImplicitAnimationCollection();
+                _implicitAnimations[nameof(Visual.Offset)] = offsetAnimation;
+                _implicitAnimations[nameof(Visual.Size)] = sizeAnimation;
+            }
+        }
+    }
+}
---
+++ RelativePanelImplicitAnimations/VisualControl.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Numerics;
+using System.Threading.Tasks;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Hosting;
+using Robmikh.CompositionSurfaceFactory;
+
+namespace RelativePanelImplicitAnimations
+{
+    public class VisualControl : UserControl
+    {
+        public static readonly DependencyProperty ImageUriProperty = DependencyProperty.Register(
+            "ImageUri", typeof(Uri), typeof(VisualControl), new PropertyMetadata(default(Uri), ImageUriChanged));
+
+        private readonly Compositor _compositor;
+
+        public VisualControl()
+        {
+            _compositor = ElementCompositionPreview.GetElementVisual(this).Compositor;
+            Visual = _compositor.CreateSpriteVisual();
+            ElementCompositionPreview.SetElementChildVisual(this, Visual);
+            SizeChanged +=
+                (sender, args) =>
+                {
+                    Visual.Size = new Vector2((float) args.NewSize.Width, (float) args.NewSize.Height);
+                };
+        }
+
+        public Uri ImageUri
+        {
+            get { return (Uri) GetValue(ImageUriProperty); }
+            set { SetValue(ImageUriProperty, value); }
+        }
+
+        public SpriteVisual Visual { get; }
+
+        private static void ImageUriChanged(DependencyObject dependencyObject,
+            DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var instance = (VisualControl) dependencyObject;
+            var unused = instance.LoadImage(dependencyPropertyChangedEventArgs.NewValue as Uri);
+        }
+
+        public async Task LoadImage(Uri imageUri)
+        {
+            try
+            {
+                var sf = SurfaceFactory.CreateFromCompositor(_compositor);
+                var surface = await sf.CreateSurfaceFromUriAsync(imageUri);
+                var brush = _compositor.CreateSurfaceBrush(surface);
+                brush.Stretch = CompositionStretch.Uniform;
+                Visual.Brush = brush;
+                Visual.Size = new Vector2((float) ActualWidth, (float) ActualHeight);
+            }
+            catch (Exception ex)
+            {
+                var m = ex.Message;
+            }
+        }
+    }
+}
